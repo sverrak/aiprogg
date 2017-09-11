365a961390a06ced7ea0a08285dbb6885eac9251
sverrak <sverre_rosicky@hotmail.com>
2017-09-11 10:33:07 +0200

    removed unused files

--- module1/input_handling.py
+++
@@ -1,3 +0,0 @@
-def read_board_from_file(input_file):
-	with open(input_file, 'r') as f:
-    	board = f.readlines()
--- module1/vehicle.py
+++
@@ -1,8 +0,0 @@
-class vehicle(object):
-
-
-	if truck:
-		LENGTH = 3
-
-	else if car:
-		length = 2
