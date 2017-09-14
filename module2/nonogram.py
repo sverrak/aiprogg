05f2e86f1e342a652b5458d1b11f80f51e7aea80
sverrak <sverre_rosicky@hotmail.com>
2017-09-14 16:32:58 +0200

    module2 init

--- module2/nonogram.py
+++ module2/nonogram.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+import time
+
+PRINTING_MODE = False
+
 class Segment:
 
     def __init__(self, x, y, length, direction):
@@ -49,31 +54,292 @@ class Row:
         self.length = length
         self.patterns = []
 
-    def generate_patterns(self, pattern):
+    # Generate the variable domain for each row
+    def generate_patterns(self, pattern, size):
+        number_of_filled_cells = sum(pattern)
+        number_of_empty_cells = size - number_of_filled_cells
+        number_of_segments = len(pattern)
+
+
+        # List of legal patterns
+        legal_patterns = []
+
+        number_of_patterns_to_be_generated = compute_number_of_patterns(pattern,size)
+        # While there are other patterns to be generated
+        while True:
+
+            temp_row = []
+            
+            # Generate
+            for n in number_of_segments:
+                continue
+            break
+        return 0
+
+
+
+    def compute_number_of_patterns(self, pattern, size):
+        if (len(pattern)==1):
+            return max(size - pattern[0] + 1, 0)
+        elif size < sum(pattern)+len(pattern) - 1:
+            return 0
+        else:
+            temp_sum = 0
+            for i in range(pattern[0], size - (sum(pattern[1:])+len(pattern[1:]))):
+                print(i)
+                temp_sum += self.compute_number_of_patterns(pattern[1:], size - i)
+            return temp_sum
+
+    def create_patterns(self, pattern, size, is_last=False, is_length_one=False):
+
+        if (len(pattern)==1):
+            patterns = []
+            temp_pattern = []
+            for i in range(size-pattern[0]+1):
+                if(is_last == True):
+                    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0 for k in range(size-i-pattern[0])])    
+                #elif(is_length_one == True):
+                #    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0 for k in range(size-i-pattern[0])])    
+                else: 
+                    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0])#+ [0 for k in range(i+1, size-pattern[0]+1)])
+            return patterns
+        elif size < sum(pattern)+len(pattern) - 1:
+            return [-1]
+        else:
+            patterns = []
+            temp_pattern = []
+
+            prefixes = self.create_patterns([pattern[0]], size - (len(pattern[1:]) + sum(pattern[1:])))
+            
+
+            for i in range(len(prefixes)):
+                if(len(pattern[1:]) == 1):
+                    tail_patterns = self.create_patterns(pattern[1:], size - len(prefixes[i]), True)
+                else:
+                    tail_patterns = self.create_patterns(pattern[1:], size - len(prefixes[i]))
+                for j in range(len(tail_patterns)):
+                    print("HEAD: " + str(prefixes[i])+ " - TAIL: " + str(tail_patterns[j]))
+                    patterns.append(prefixes[i] + tail_patterns[j])
+
+            return patterns
+
+    def pattern_generation_helper(pattern, size):
+        return 0
 
 
 
 
+# ******* A* HELP FUNCTIONS *********
+
+# *** Problem dependent ***
+
+def move(args):
+    
+    return 0
+
+# *** Problem dependent ***
+# Checks whether a certain state is the target state or not
+def is_finished_state(args):
+
+    return 0
+    
+# *** Problem dependent ***
+# Returns all possible neighbor states and which move that has been done to get there
+def generate_successors(current_state):
+
+    return 0
+
+# *** Problem dependent ***
+# Computing the heuristic cost of a certain state
+# Not updated to handle both columns AND rows: currently only rows 
+def estimate_cost(rows, mode, column_domain):
+    if (mode == "bfs" or mode == "dfs"):
+        return 0
+    elif (mode == "A*"):
+
+        # If A* is chosen, we compute the degree to which each column constraints is violated
+        columns = map(list, zip(*rows))
+        print(rows)
+        print(columns)
+        cost = 0
+
+        # For each column, compute the lowest 
+        for i in range(len(columns)):
+            cost += compute_lowest_error(columns[i], column_domain[i])
+        
+        return cost
+
+# Returns the most similar row within the column_domain to the input column and the corresponding Manhattan distance
+# TO DO: Not sure if the most similar row is needed
+def compute_lowest_error(column, column_domain):
+    closest_legal_column = []
+    distance_to_closest_legal_column = 9999999
+
+    for cd in range(len(column_domain)):
+        temp_distance = 0
+
+        for i in range(len(column)):
+            temp_distance += 1 if (column_domain[cd][i] != column[i]) else 0
+
+        if temp_distance < distance_to_closest_legal_column:
+            distance_to_closest_legal_column = temp_distance
+            closest_legal_column = column_domain[cd]
+    print("Column domain: " + str(column_domain))        
+    print("Closest legal column to " + str(column) + " is " + str(closest_legal_column) + " with cost " + str(distance_to_closest_legal_column) + "\n")
+    return distance_to_closest_legal_column
+
+# Returns the variable domain for a certain pattern (row constraints) 
+def create_patterns(pattern, size, is_last=False, is_length_one=False):
+        if (len(pattern)==1):
+            patterns = []
+            temp_pattern = []
+            for i in range(size-pattern[0]+1):
+                if(is_last == True):
+                    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0 for k in range(size-i-pattern[0])])    
+                #elif(is_length_one == True):
+                #    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0 for k in range(size-i-pattern[0])])    
+                else: 
+                    patterns.append([0 for j in range(0,i)] + [1 for l in range(pattern[0])] + [0])#+ [0 for k in range(i+1, size-pattern[0]+1)])
+            return patterns
+        elif size < sum(pattern)+len(pattern) - 1:
+            return [-1]
+        else:
+            patterns = []
+            temp_pattern = []
+
+            prefixes = create_patterns([pattern[0]], size - (len(pattern[1:]) + sum(pattern[1:])))
+            
+
+            for i in range(len(prefixes)):
+                if(len(pattern[1:]) == 1):
+                    tail_patterns = create_patterns(pattern[1:], size - len(prefixes[i]), True)
+                else:
+                    tail_patterns = create_patterns(pattern[1:], size - len(prefixes[i]))
+                for j in range(len(tail_patterns)):
+                    patterns.append(prefixes[i] + tail_patterns[j])
+
+            return patterns
+
+# Not implemented
+# Is this needed?
+def from_rows_to_board():
+
+    return 0
+
+# Heuristic function to initiate the best possible starting rows
+# Currently, this method simply returns the first pattern in the pattern domain. 
+# Should be improved
+def init_rows(patterns_input, size):
+    rows = []
+    for i in range(size):
+        patterns = create_patterns(patterns_input[i], size, len(patterns_input[i])==1, len(patterns_input[i])==1)
+        rows.append(patterns[0])
+
+    return rows
+
+# Not yet completed
+# Returns the size of the nonogram as well as the row and column constraints
+def generate_pattern_input_from_file(input_file):
+
+    with open(input_file, 'r') as f:
+        raw = f.readlines()
+
+        firstline = raw[0].split(" ")
+        print(firstline)
+        sizeX, sizeY = int(firstline[0]), int(firstline[1])
+
+        # Put the information on the right containers
+        row_patterns_input = raw[1:1+sizeX]
+        row_patterns_input.reverse()
+        col_patterns_input = raw[1+sizeX:]
+
+        print(row_patterns_input)
+        print(col_patterns_input)
+
+        # Transform the data to list of lists of integers
+        # Not working
+        for i in range(len(row_patterns_input)):
+            for j in range(len(row_patterns_input[i])):
+                row_patterns_input[i][j] = int(row_patterns_input[i][j])
+
+        for i in range(len(col_patterns_input)):
+            for j in range(len(col_patterns_input[i])):
+                col_patterns_input[i][j] = int(col_patterns_input[i][j])
+
+        return sizeX, sizeY, row_patterns_input, col_patterns_input
+
+# To be implemented
+# Could we somehow use entropy gain here?
+# Determines the variable on which to base the next assumption.
+def find_next_row(domains):
+    # Here, the fitness of a candidate row or column is the degree to which the row or column splits 
+
+    best_row = -1
+    best_fitness = 999999
+
+    return 0
 
 
+def revise()
+
+# To be implemented
+# Solving the problem using the A* algorithm
+def solve():
+    
+    return 0
+
+def main():
+    print("\n************************************\n************************************")
+    #print("Level: " + str(LEVEL))
+    #print("Algorithm: " + MODE + "\n")
 
 
+    # Initializing the program
+    #start = time.time()
+    #sizeX, sizeY, row_patterns_input, col_patterns_input = generate_pattern_input_from_file("cat.txt")
+    
+    sizeX = 4
+    sizeY = 4
+    rows = []
+
+    row_patterns_input = [[2], [1, 1], [3], [2, 1]]
+    col_patterns_input = [[1, 1], [2], [2, 1], [3]]
+    column_patterns = []
+
+    for i in range(len(col_patterns_input)):
+        column_patterns.append(create_patterns(col_patterns_input[i], sizeY, len(col_patterns_input[i])==1, len(col_patterns_input[i])==1))
+    
+    print("Col pat: " + str(column_patterns))
+    
 
 
+    initial_rows = init_rows(row_patterns_input, sizeX)
+    print(initial_rows)
+    print(estimate_cost(initial_rows, "A*", column_patterns))
 
 
+    end = time.time()
+
+    # Displaying run characteristics
+    if (PRINTING_MODE == True):
+        print("RUNTIME: " + str(end - start) + "\n")
 
 
+    # Solving the puzzle
+    if (False):
+        moves, final_rows = solve(initial_rows, MODE)
+        board = from_rows_to_board(final_rows)
+        
+        
+        if (DISPLAY_MODE == True):
+            visualize_development1(vehicles, moves)
+        else:
+            print("\n\n\n***LEVEL SOLVED***")
+            print_board(board)
+
+pattern = [2,1]
+#print("RESULT: " + str(compute_number_of_patterns(pattern, 7, False, len(pattern)==1)))
+print("Create_patterns: " + str(create_patterns(pattern, 5, len(pattern)==1, len(pattern)==1)))
 
 
-
-
-
-
-
-
-
-
-
-
-
+#main()
